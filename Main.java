047a5a2636577adbc22e3c1ef3c315d0d013e212
Mauricepns <66377105+Mauricepns@users.noreply.github.com>
2020-06-14 16:21:59 +0200

    Mastermind V.1.0

--- Main.java
+++ Main.java
@@ -11,34 +11,41 @@ public class Main {
         Random r = new Random();
         boolean spelen = true;
         String stoppen = "q";
+        String goedeLetters = "abcdef";
 
         char a = (char) (r.nextInt(6) + 'a');
         char b = (char) (r.nextInt(6) + 'a');
         char c = (char) (r.nextInt(6) + 'a');
         char d = (char) (r.nextInt(6) + 'a');
 
-        System.out.println("het antwoord is " + a + b + c + d);
-
+//        System.out.println("het antwoord is " + a + b + c + d); // Goede antwoord
+        System.out.println("");
+        System.out.println("*********************");
         System.out.println("Welkom bij Mastermind");
+        System.out.println("*********************");
+        System.out.println("");
 
         while (spelen) {
 
             int aantalgoed = 0;
             int aantalgoedFout = 0;
 
-            System.out.println("Voer je antwoord in, of q om te stoppen ");
+            System.out.println("Voer 4 letters (a t/m f) om te raden, of q om te stoppen ");
             String invoer = scanner.nextLine();
+            String invoerLowerCase = invoer.toLowerCase();
 
             if (invoer.equals(stoppen)) {
                 spelen = false;
                 System.out.println("Bedankt voor het spelen");
+            }
 
-            } else {
-                System.out.println(invoer);
-                char a1 = invoer.charAt(0);
-                char b1 = invoer.charAt(1);
-                char c1 = invoer.charAt(2);
-                char d1 = invoer.charAt(3);
+            if ((invoerLowerCase.length() == 4) && (invoerLowerCase.matches("[" + goedeLetters + "]+"))) {
+
+//              System.out.println(invoerLowerCase); print check van invoer
+                char a1 = invoerLowerCase.charAt(0);
+                char b1 = invoerLowerCase.charAt(1);
+                char c1 = invoerLowerCase.charAt(2);
+                char d1 = invoerLowerCase.charAt(3);
 
                 if (a == a1 && b == b1 && c == c1 && d == d1) {
                     System.out.println("correct, je hebt gewonnen!");
@@ -68,6 +75,11 @@ public class Main {
                 }
                 System.out.println("Je hebt er " + aantalgoed + " op de goede plek");
                 System.out.println("Je hebt er " + aantalgoedFout + " goed, maar op de verkeerd plek");
+                System.out.println("");
+            }
+            else {
+                System.out.println("Invoer bevat ongeldige letters of aantal ");
+                System.out.println("");
             }
         }
     }
